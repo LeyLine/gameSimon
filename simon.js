c0d4c9c238e4c1617243e4eea5bf3a145cfb3462
Adrian Llerena <a.llerena001@gmail.com>
2018-01-22 08:56:25 -0500

    css done

---
+++ simon.js
@@ -0,0 +1,9 @@
+function SimonGame () {
+  this.colors         = ["red", "green", "blue", "yellow"];
+ this.sequence       = [];
+ this.userClickCount = 0;
+ this.round          = 1;
+}
+
+SimonGame.prototype.startGame = function () {
+};
